29cc2b850159fbc183e17ca9cb72a951f8b04317
Gaston Landeira <gastonlandeira@gmail.com>
2024-12-10 18:00:39 -0300

    database setup

---
+++ patient-registration/database/factories/PatientFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Patient>
+ */
+class PatientFactory extends Factory
+{
+    protected $model = Patient::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'document_photo' => $this->faker->imageUrl(640, 480, 'business', true, 'Faker'),
+        ];
+    }
+}
